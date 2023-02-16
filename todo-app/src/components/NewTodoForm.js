0aab90d45666d62b0cdb43c6e20717f778e6b632
Konul304 <memmedovakonul270@gmail.com>
2023-02-16 11:43:26 +0400

    feat: add with enter key

--- todo-app/src/components/NewTodoForm.js
+++ todo-app/src/components/NewTodoForm.js
@@ -1,13 +1,19 @@
 const NewTodoForm = ({ value, updateText, handleAction }) => {
+  const handleKeypress = e => {
+  if (e.code === "Enter") {
+    handleAction();
+  }
+};
     return (
       <label className="d-flex align-items-baseline justify-content-center">
         <input 
         className="mt-5 me-2 p-1"
           placehodler='new todo'
           value={value}
+          onKeyPress={handleKeypress}
           onChange={(e) => updateText(e.target.value)}
         />
-        <button className="btn btn-primary" onClick={handleAction}>Add to do</button>
+        <button className="btn btn-primary"  onClick={handleAction}>Add to do</button>
       </label>
     );
   };
